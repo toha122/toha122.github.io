e9f1cefa7173912a466e70dc7319ab887e0babbb
Sapizhak Antonina <147391830+toha122@users.noreply.github.com>
2024-10-31 20:54:09 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,9 +2,9 @@ window.onload = function () {
     const progressBars = document.querySelectorAll('.progress-fill');
     progressBars.forEach(bar => {
         const width = bar.style.width;
-        bar.style.width = '0'; 
+        bar.style.width = '0';
         setTimeout(() => {
-            bar.style.width = width; 
+            bar.style.width = width;
         }, 500);
     });
 };
@@ -19,7 +19,3 @@ document.querySelectorAll('a[data-section]').forEach(link => {
         }
     });
 });
-
-function toggleMenu() {
-    document.querySelector('.nav').classList.toggle('active'); 
-}
